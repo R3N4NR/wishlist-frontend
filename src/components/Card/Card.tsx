f59371380001b4d5a431516b7fe27a0d25346d4d
R3N4NR <renanramalhou@gmail.com>
2024-03-06 23:04:57 -0300

    ajusta o componente card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,4 @@
+// Card.js
 import styles from './styles.module.scss';
 import { Pedidos, Rotas, Cartinhas } from '../../interfaces/interfaces';
 import { useEffect, useState } from 'react';
@@ -9,10 +10,26 @@ interface CardProps {
 
 const Card = ({ type, data }: CardProps) => {
     const [cardInfo, setCardInfo] = useState<Cartinhas | Pedidos | Rotas>()
+    
+    useEffect(() => {
+        const handleData = () => {
+            if(type === 'Cartinhas'){
+                setCardInfo(data as Cartinhas)
+            }
+            if(type === 'Pedidos'){
+                setCardInfo(data as Pedidos)
+            }
+            if(type === 'Rotas'){
+                setCardInfo(data as Rotas)
+            }
+        }
 
+        handleData()
+    }, [data, type])
+    console.log((data as Pedidos).descricao , type)
     return (
         <div className={styles.card}>
-            
+            <p>{cardInfo?.nome}</p>
         </div>
     );
 };
